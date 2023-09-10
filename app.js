a2c31f9ded9140c7852e0bf7bf2acd2568327af6
Russell <aminulislamrussell3@gmail.com>
2023-09-11 01:37:04 +0600

    first commit

--- app.js
+++ app.js
@@ -1,43 +1,35 @@
-const faqs = document.querySelectorAll(".faq_wrapper");
+const faqs = document.querySelectorAll('.faq_wrapper');
 faqs.forEach((faq) => {
-  faq.addEventListener("click", () => {
+  faq.addEventListener('click', () => {
     faqs.forEach((faq) => {
-      faq.classList.remove("faq_active");
-      faq.classList.add("faq_active");
+      faq.classList.remove('faq_active');
+      faq.classList.add('faq_active');
     });
-    faq.classList.toggle("faq_active");
+    faq.classList.toggle('faq_active');
   });
 });
-const ben_cards = document.querySelectorAll(".demo_details_card");
+const ben_cards = document.querySelectorAll('.demo_details_card');
 ben_cards.forEach((item) => {
-  item.addEventListener("click", () => {
+  item.addEventListener('click', () => {
     ben_cards.forEach((item) => {
-      item.classList.remove("demo_details_card_active");
+      item.classList.remove('demo_details_card_active');
     });
-    item.classList.add("demo_details_card_active");
+    item.classList.add('demo_details_card_active');
   });
 });
 
 // Toggle Mobile Menu
-const menuBtn = document.querySelector(".mobile_menu_icon");
-const nav = document.querySelector(".nav");
+const menuBtn = document.querySelector('.mobile_menu_icon');
+const nav = document.querySelector('.nav');
 
-menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("nav_active");
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav_active');
 
-  if (menuBtn.classList.contains("icofont-navigation-menu")) {
-    menuBtn.classList.remove("icofont-navigation-menu");
-    menuBtn.classList.add("icofont-close");
+  if (menuBtn.classList.contains('icofont-navigation-menu')) {
+    menuBtn.classList.remove('icofont-navigation-menu');
+    menuBtn.classList.add('icofont-close');
   } else {
-    menuBtn.classList.add("icofont-navigation-menu");
-    menuBtn.classList.remove("icofont-close");
+    menuBtn.classList.add('icofont-navigation-menu');
+    menuBtn.classList.remove('icofont-close');
   }
 });
-
-const names = [" A", "B", "C", "D", "E", "F", "G"];
-
-names.forEach(function (number) {
-  console.log(number);
-});
-
-// slider
